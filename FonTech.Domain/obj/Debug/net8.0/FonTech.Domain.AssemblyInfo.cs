8a61eb633a392df20ede998deb8f4df777a0d93e
Андрей Якимов <leowarp@bk.ru>
2025-01-23 13:59:15 +0300

    Commit 1

--- FonTech.Domain/obj/Debug/net8.0/FonTech.Domain.AssemblyInfo.cs
+++ FonTech.Domain/obj/Debug/net8.0/FonTech.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FonTech.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ee989ea9fd3473d2610afbedbd9166635a948d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("FonTech.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FonTech.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
